43c01565e60d2399e5467c3b467811771e37adfd
zhangxinyu <zxy_email@qq.com>
2019-05-07 02:02:07 +0800

    commit

---
+++ build/precache-manifest.38c57a1670c67b03b384438f9ab5b85e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b5a56146323c8bd693baa90b73222c0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f89f60e2ac43d3ecda6f",
+    "url": "/static/css/2.43e62e34.chunk.css"
+  },
+  {
+    "revision": "0e8275d290e64bbf0dd9",
+    "url": "/static/css/main.795991f2.chunk.css"
+  },
+  {
+    "revision": "f89f60e2ac43d3ecda6f",
+    "url": "/static/js/2.26de6ea7.chunk.js"
+  },
+  {
+    "revision": "0e8275d290e64bbf0dd9",
+    "url": "/static/js/main.99564934.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "0379fcf0efe0e07d08db8a220f635054",
+    "url": "/static/media/editor.0379fcf0.less"
+  }
+]);
